c86748d75bfc20cf09c7250db14b8d0631658f70
junseok <junseok.oh@gentlepie.com>
2023-10-19 16:25:02 +0900

    Fix: 소켓 유저별

--- src/chat/dto/create-chat.dto.ts
+++ src/chat/dto/create-chat.dto.ts
@@ -1,6 +1,16 @@
-import { IsString } from "class-validator";
+import { IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreateChatDto {
   @IsString()
   message: string;
+
+  @IsNumber()
+  userId: number;
+
+  @IsString()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  room: string;
 }
