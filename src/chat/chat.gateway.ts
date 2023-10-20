a5735cfcbc60c237a9735767eed54b041f48c011
junseok <junseok.oh@gentlepie.com>
2023-10-20 11:07:41 +0900

    Feat: 채팅시 들어오는 user check

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -3,16 +3,21 @@ import { CreateChatDto } from './dto/create-chat.dto';
 import { Server } from 'socket.io';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { ChatService } from './chat.service';
 
 @WebSocketGateway({ cors: true })
 export class ChatGateway {
+  constructor(
+    private readonly chatService: ChatService,
+  ) {}
+
   @WebSocketServer()
   server: Server;
 
-
   @SubscribeMessage('message')
   @UseGuards(AuthGuard)
-  chatting(@MessageBody() createChatDto: CreateChatDto) {
+  async chatting(@MessageBody() createChatDto: CreateChatDto) {
+    await this.chatService.userCheck(createChatDto.userId);
     this.server.emit('message', createChatDto);
   }
 }
