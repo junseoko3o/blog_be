3a0ca00296ad7d38cf6f53f03c62dea3a996b0d4
junseok <junseok.oh@gentlepie.com>
2023-10-20 11:07:41 +0900

    Feat: 채팅시 들어오는 user check

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class ChatService {
+  constructor(
+    private readonly userService: UserService,
+  ) {}
+
+  async userCheck(userId: number) {
+    return await this.userService.findOneUser(userId);
+  }
 }
