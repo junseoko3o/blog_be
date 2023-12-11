e8639a88a96a6c7da010098fba5b4bdb99830232
o3ojunseok <o3ojunseok@gmail.com>
2023-12-11 23:10:57 +0900

    Feat: Health Check:

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,37 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator, MemoryHealthIndicator, TypeOrmHealthIndicator } from "@nestjs/terminus";
+import { Public } from './auth/public.decorator';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly health: HealthCheckService,
+    private readonly http: HttpHealthIndicator,
+    private readonly db: TypeOrmHealthIndicator,
+    private readonly memory: MemoryHealthIndicator,
+    @InjectDataSource()
+    private readonly defaultDataSource: DataSource,
+    ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
   
+  @Get('health')
+  @HealthCheck()
+  @Public()
+  check() {
+      return this.health.check([
+          () => this.http.pingCheck('mango-admin', process.env.HEALTH_CHECK),
+          () => this.http.pingCheck('mango-chathot', process.env.HEALTH_CHECK_CHATBOT), 
+          () => this.db.pingCheck('database', { connection: this.defaultDataSource }),
+          () => this.memory.checkHeap('memory heap', 300 * 1024 * 1024),
+          () => this.memory.checkRSS('memory RSS', 300 * 1024 * 1024),
+      ])
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from './auth/guard/jwt-auth.guard';
 import { HttpExceptionFilter } from './common/exception/exception.filter';
 import * as cookieParser from 'cookie-parser';
-import { RedisIoAdapter } from './chat/redis.adapter';
+// import { RedisIoAdapter } from './chat/redis.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
