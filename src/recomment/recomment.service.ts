a31a2d6d7322ebef7674b277f1cab6910944dc25
o3ojunseok <o3ojunseok@gmail.com>
2024-01-23 23:20:46 +0900

    Feat: entity relationship

--- src/recomment/recomment.service.ts
+++ src/recomment/recomment.service.ts
@@ -2,9 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { CreateRecommentDto } from './dto/create-recomment.dto';
 import { UpdateRecommentDto } from './dto/update-recomment.dto';
 import { UserService } from 'src/user/user.service';
+import { ContentService } from 'src/content/content.service';
+import { CommentService } from 'src/comment/comment.service';
 
 @Injectable()
 export class RecommentService {
   constructor(
+    private readonly userService: UserService,
+    private readonly contentService: ContentService,
+    private readonly commentService: CommentService,
   ) {}
 }
