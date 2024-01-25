dee1389a21b2d0557721d1bc3293c52b8a8ab439
o3ojunseok <o3ojunseok@gmail.com>
2024-01-25 17:49:41 +0900

    Feat: create dto

--- src/recomment/dto/create-recomment.dto.ts
+++ src/recomment/dto/create-recomment.dto.ts
@@ -1,1 +1,15 @@
-export class CreateRecommentDto {}
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateRecommentDto {
+  @IsString()
+  recomment: string;
+
+  @IsNumber()
+  content_id: number;
+
+  @IsNumber()
+  comment_id: number;
+
+  @IsNumber()
+  created_user_id: number;
+}
