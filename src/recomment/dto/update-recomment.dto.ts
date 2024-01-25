67556c48ce8979b30e1daa34dba0031ce8fab8e0
o3ojunseok <o3ojunseok@gmail.com>
2024-01-25 17:57:23 +0900

    Feat: update recomment dto

--- src/recomment/dto/update-recomment.dto.ts
+++ src/recomment/dto/update-recomment.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRecommentDto } from './create-recomment.dto';
+import { IsNumber, IsString } from 'class-validator';
 
-export class UpdateRecommentDto extends PartialType(CreateRecommentDto) {}
+export class UpdateRecommentDto extends PartialType(CreateRecommentDto) {
+  @IsString()
+  recomment: string;
+
+  @IsNumber()
+  content_id: number;
+
+  @IsNumber()
+  comment_id: number;
+
+  @IsNumber()
+  updated_user_id: number;
+}
