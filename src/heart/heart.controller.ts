50962954833ca3a95d135ce985a162651e421ce5
o3ojunseok <o3ojunseok@gmail.com>
2024-02-09 23:03:13 +0900

    Feat: heart in recomment & comment

--- src/heart/heart.controller.ts
+++ src/heart/heart.controller.ts
@@ -4,4 +4,14 @@ import { HeartService } from './heart.service';
 @Controller('heart')
 export class HeartController {
   constructor(private readonly heartService: HeartService) {}
+
+  @Get('/comment/:comment_id')
+  async findOneHeartInComment(@Param('comment_id') comment_id: number) {
+    return await this.heartService.findOneHeartInComment(comment_id);
+  }
+
+  @Get('/recomment/:recomment_id')
+  async findOneHeartInRecomment(@Param('recomment_id') recomment_id: number) {
+    return await this.heartService.findOneHeartInRecomment(recomment_id);
+  }
 }
