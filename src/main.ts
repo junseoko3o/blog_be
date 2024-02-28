04f9cb3c5a2ccf53972a8cd7e1d5864a88bb111d
o3ojunseok <o3ojunseok@gmail.com>
2024-02-28 22:59:35 +0900

    Fix: Fix

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from './auth/guard/jwt-auth.guard';
 import { HttpExceptionFilter } from './common/exception/exception.filter';
-import * as cookieParser from 'cookie-parser';
 import { RedisIoAdapter } from './chat/redis.adapter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
