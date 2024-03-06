96d374140a0d4fc80c9f7b24f5349d27002ca33a
o3ojunseok <o3ojunseok@gmail.com>
2024-03-06 16:31:21 +0900

    Feat: 좋아요 정보

--- src/heart/dto/comment-heart-info.dto.ts
+++ src/heart/dto/comment-heart-info.dto.ts
@@ -1,9 +1,9 @@
 import { IsBoolean, IsNumber } from "class-validator";
 
-export class UpdateCommentHeartDto {
+export class CommentHeartInfoDto {
   @IsNumber()
   comment_id: number;
 
-  @IsBoolean()
-  like: boolean;
+  @IsNumber()
+  user_id: number;
 }
