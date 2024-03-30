9698a904ec77239bdfc6b0a35a29be08448a2d94
o3ojunseok <o3ojunseok@gmail.com>
2024-03-30 23:54:18 +0900

    Feat: Controller response dto type

--- src/user/responseDto/create-user.response.dto.ts
+++ src/user/responseDto/create-user.response.dto.ts
@@ -1,3 +1,23 @@
+import { IsDate, IsEmail, IsNumber, IsString } from "class-validator";
 
-export class CreateUserResponseDto {
+export class UserResponseDto {
+  @IsNumber()
+  id: number;
+
+  @IsEmail()
+  user_email: string;
+
+  @IsString()
+  user_name: string;
+
+  @IsString()
+  password: string;
+
+  @IsDate()
+  login_at: Date;
+
+  @IsDate()
+  created_at: Date;
+
+  updated_at: Date;
 }
